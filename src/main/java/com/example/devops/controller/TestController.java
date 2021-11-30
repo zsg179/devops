39e46cb404a654db712425534b1da596d910f770
朱树广 <zsg179@163.com>
2021-11-30 17:25:58 +0800

    first

---
+++ src/main/java/com/example/devops/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.devops.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @description:
+ * @author: sg zhu
+ * @create: 2021-11-30 16:05
+ */
+@Controller
+public class TestController {
+
+    public void test() {
+
+    }
+}
