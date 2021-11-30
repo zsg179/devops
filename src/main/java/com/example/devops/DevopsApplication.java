c564c76a8eb43d086475fb97d252dd6776f2a7a5
朱树广 <zsg179@163.com>
2021-11-30 14:58:43 +0800

    first

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DevopsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApplication.class, args);
+	}
 
 }
